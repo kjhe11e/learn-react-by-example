0bddf5f1556b96258b37c1685f9d5802f8599478
Kyle Helle <kjhelle4@gmail.com>
2020-02-21 08:26:35 -0800

    initial commit

---
+++ 01-movies-menu/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/list';
+
+const App = () => {
+  return <List />;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
